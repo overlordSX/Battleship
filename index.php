498c2ef5516e08859c10463681482cd609b4cc30
andrey.solovev <stajer@sibirix.ru>
2022-07-05 18:10:44 +0700

    + добавил разметку html страницы

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Список товаров</title>
+</head>
+<body>
+
+
 <?php
+include("database/connection.php");
+?>
+
+
+
+
+</body>
+</html>
+
