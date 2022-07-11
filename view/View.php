6343e037f1a32b8f4aab10c155b55075f7621597
andrey.solovev <stajer@sibirix.ru>
2022-07-11 14:42:58 +0700

    * убрал лишний метод

--- view/View.php
+++ view/View.php
@@ -2,14 +2,9 @@
 
 class View
 {
-    public function generate(string $filepath, array $data = []): void
+    public function generateView(string $filepath, array $data = []): void
     {
         extract($data);
         include $filepath;
-    }
-
-    public static function getHeader(): void
-    {
-        //include
     }
 }
