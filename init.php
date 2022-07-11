179aad5442a8172fd1599f57d8adbc914abb06d2
andrey.solovev <stajer@sibirix.ru>
2022-07-11 14:42:09 +0700

    + вынес все подключения файлов в отдельный файл, чтобы подключать все единовременно

---
+++ init.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "routes/Router.php";
+require_once "routes/routes.php";
+
+require_once "database/entity/AbstractEntity.php";
+require_once "database/entity/ProductEntity.php";
+require_once "database/entity/CommentEntity.php";
+
+require_once "database/util/EntityUtil.php";
+
+require_once "database/Products.php";
+require_once "database/Comments.php";
+
+require_once "database/config/config.php";
+require_once "database/Database.php";
+
+require_once "view/View.php";
+
+require_once "controllers/CatalogController.php";
