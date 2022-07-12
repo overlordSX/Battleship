c6ce627e73121c33f0123640fee0f54e95700ba1
andrey.solovev <stajer@sibirix.ru>
2022-07-12 17:20:13 +0700

    + функционал сортировки по трем параметрам * немного изменил метод генерации товаров

--- controllers/CatalogController.php
+++ controllers/CatalogController.php
@@ -4,15 +4,42 @@ class CatalogController
 {
     public const PAGE_SIZE = 5;
 
-    public static function renderPage($currentPageNumber = 1): void
+    public static function renderPage($pageUrl, $currentPageNumber = 1, $sortParam = "", $order = 'asc'): void
     {
+        //todo №1 что делать с тем когда таблицы нет
         Products::createTable();
+        Comments::createTable();
+        $productList = [];
         $totalProducts = Products::getCountOfProducts();
         $offset = ($currentPageNumber - 1) * self::PAGE_SIZE;
-        $productList = Products::selectProductsLimitOffset(
-            self::PAGE_SIZE,
-            $offset
-        );
+        if ($sortParam !== "") {
+            if ($sortParam === 'name') {
+                $productList = Products::selectProductsWithQuantityOfCommentsSortByNameLimitOffset(
+                    self::PAGE_SIZE,
+                    $offset,
+                    $order
+                );
+            } elseif ($sortParam === 'price') {
+                $productList = Products::selectProductsWithQuantityOfCommentsSortByPriceLimitOffset(
+                    self::PAGE_SIZE,
+                    $offset,
+                    $order
+                );
+            } elseif ($sortParam === 'comments') {
+                echo " comments ";
+                $productList = Products::selectProductsWithQuantityOfCommentsSortByCommentsLimitOffset(
+                    self::PAGE_SIZE,
+                    $offset,
+                    $order
+                );
+            }
+        } else {
+            $productList = Products::selectProductsWithQuantityOfCommentsLimitOffset(
+                self::PAGE_SIZE,
+                $offset
+            );
+        }
+
 
         $view = new View();
         $view->generateView('./view/catalog/catalog.php',
@@ -20,7 +47,8 @@ class CatalogController
                 'productList' => $productList,
                 'currentPageNumber' => $currentPageNumber,
                 'totalProducts' => $totalProducts,
-                'countOfPages' => ceil($totalProducts / self::PAGE_SIZE)
+                'countOfPages' => ceil($totalProducts / self::PAGE_SIZE),
+                'pageUrl' => $pageUrl
             ]
         );
     }
@@ -28,16 +56,17 @@ class CatalogController
     public static function dropTable(): void
     {
         Products::dropTable();
+
+        header('Location: /');
+        die();
     }
 
     public static function createTable(): void
     {
         Products::createTable();
-        self::renderPage();
-    }
+        header('Location: /');
+        die();
 
-    public static function createNewProduct(): void
-    {
     }
 
     public static function generateProducts($quantity): void
@@ -47,15 +76,17 @@ class CatalogController
         for ($i = 1; $i <= $quantity; $i++) {
             $product = new ProductEntity(
                 [
-                    'name' => "milk " . $i,
-                    'description' => $i . " %",
-                    'price' => $i,
+                    'name' => "Товар " . rand(0, 50000),
+                    'description' => "Описание " . $i,
+                    'price' => rand(0, 50000),
                     'id' => null
                 ]
             );
 
             Products::insertNewProduct($product);
         }
+        header('Location: /');
+        die();
 
     }
 }
