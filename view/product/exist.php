b58c6e462656dc27ba3c38e458c3e2fba740032b
andrey.solovev <stajer@sibirix.ru>
2022-07-12 17:28:15 +0700

    + вью, отвечающая за отображение товара с формой добавления отзыва, и уже добавленных отзывов

---
+++ view/product/exist.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @var ProductEntity $product
+ * @var CommentEntity[] $commentsList
+ * @var int $commentPage
+ * @var int $totalPages
+ */
+
+$headerView = new View();
+$headerView->generateView(
+    'view/layouts/header.php',
+    ['title' => "Товар №" . $product->getId()]
+);
+?>
+
+    <div class="container">
+    <div class="row">
+        <div class="col p-2">
+            <div class="card">
+                <div class="card-body">
+                    <div class="p-3">
+                        <h5 class="card-title"><?= $product->getName() ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?= $product->getPrice() ?></h6>
+                        <p class="card-text"><?= $product->getDescription() ?></p>
+                    </div>
+                    <hr>
+
+                    <div class="row row-cols-auto justify-content-center">
+                        <div class="col">
+                            <a href="/catalog/">
+                                <button type="button" class="btn btn-outline-primary">Каталог</button>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a href="/">
+                                <button type="button" class="btn btn-outline-secondary">Главная</button>
+                            </a>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php
+$commentView = new View();
+$commentView->generateView(
+    'view/comments/comments.php',
+    [
+        'commentsList' => $commentsList,
+        'productId' => $product->getId(),
+        'commentPage' => $commentPage,
+        'totalPages' => $totalPages
+    ]
+);
+?>
+
+
+<?php
+$footerView = new View();
+$footerView->generateView(
+    'view/layouts/footer.php'
+);
+?>
