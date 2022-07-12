4f9c640aa43cdb3d8f631cbfe74d81dc9702a514
andrey.solovev <stajer@sibirix.ru>
2022-07-12 17:29:48 +0700

    + подключил bootstrap 5

--- view/layouts/header.php
+++ view/layouts/header.php
@@ -1,7 +1,14 @@
+<?php
+/**
+ * @var string $title
+ */
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
