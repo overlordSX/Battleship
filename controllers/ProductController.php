9ab85a9817535dbbb0cec2a4bc15ccc553528a94
andrey.solovev <stajer@sibirix.ru>
2022-07-13 17:23:00 +0700

    + добавил проверку на существование товара в базе + изменил метод добавления нового комментария

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -2,33 +2,46 @@
 
 class ProductController
 {
-    public static function getNewProductForm(): void
+    public static function newProduct(): void
     {
-        $createFormView = new View();
-        $createFormView->generateView('view/product/new.php');
+        if($_SERVER['REQUEST_METHOD'] === 'GET') {
+            View::generateView('view/product/new.php');
+        }
+        // todo #1 это нормально?
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+
+
+            //if (Validator::make($data))
+            //если ок добавить, иначе сообщить об ошибках
+
+
+            $product = new ProductEntity([
+                    'name' => $_POST['name'],
+                    'description' => $_POST['description'],
+                    'price' => $_POST['price']
+                ]
+            );
+            Products::insertNewProduct($product);
+
+            header('Location: /catalog/');
+        }
     }
 
-    public static function createNewProduct($name, $description, $price): void
+    public static function showProduct($productId, $pageUrl): void
     {
-        Products::createTable();
-        $product = new ProductEntity([
-                'name' => $name,
-                'description' => $description,
-                'price' => $price
-            ]
-        );
-        Products::insertNewProduct($product);
 
-        header('Location: /catalog/');
-    }
+        $parse = parse_url($pageUrl);
+        $start = $parse['query'] ?? '';
+        parse_str($start, $query);
 
-    public static function showProduct($productId, $currentCommentPage = 1): void
-    {
-        Comments::createTable();
+        $currentCommentPage = $query['page'] ?? 1;
 
-        $product = Products::selectProductWithId($productId)[0];
+        $product = Products::getProductWithId($productId);
+        if (!$product) {
+            header('Location: /error-404');
+        }
 
-        // а не нужно ли отсюда вызывать мессэдж контроллер
         $totalComments = Comments::getCountOfComments($productId);
 
         $offset = ($currentCommentPage - 1) * CommentController::COMMENTS_PAGE_SIZE;
@@ -38,14 +51,14 @@ class ProductController
             offset: $offset
         );
 
-        $showProductView = new View();
-        $showProductView->generateView(
+        View::generateView(
             "view/product/exist.php",
             [
                 'product' => $product,
                 'commentsList' => $commentsList,
                 'commentPage' => $currentCommentPage,
-                'totalPages' => ceil($totalComments / CommentController::COMMENTS_PAGE_SIZE)
+                'totalPages' => ceil($totalComments / CommentController::COMMENTS_PAGE_SIZE),
+                'query' => $query
             ]
         );
     }
