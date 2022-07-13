aa5457cb64c64f090a9dce40e7c359e10498d1b3
andrey.solovev <stajer@sibirix.ru>
2022-07-13 17:29:03 +0700

    + добавил метод проверяющий существование таблицы

--- database/Comments.php
+++ database/Comments.php
@@ -7,8 +7,8 @@ class Comments
 {
     public static function createTable(): void
     {
-        //TODO узнать нужно ли это защитить как то от атак
-        $createTableQuery = "CREATE TABLE IF NOT EXISTS `comments` 
+        $createTableQuery = "
+        CREATE TABLE IF NOT EXISTS `comments` 
         (`id` INT NOT NULL AUTO_INCREMENT, 
         `email` VARCHAR(200) NOT NULL,
         `comment` VARCHAR(500) NOT NULL,
@@ -23,15 +23,15 @@ class Comments
         Database::exec($createTableQuery);
     }
 
-    /**
-     * @return CommentEntity[] возвращает список объектов
-     */
-    public static function selectAllComments(): array
+    public static function isTableExist(): bool
     {
-        $selectAllQuery = "select * from comments;";
-        $result = Database::queryFetchAll($selectAllQuery);
+        $isExistQuery = '
+        SELECT *
+        FROM information_schema.tables 
+        WHERE table_schema = DATABASE() AND table_name = `comments`;';
 
-        return EntityUtil::resultToListOfEntities("CommentEntity", $result);
+        var_dump(Database::queryFetchRow($isExistQuery));
+        return Database::queryFetchRow($isExistQuery);
     }
 
     public static function getCountOfComments($productId): int
@@ -40,15 +40,7 @@ class Comments
         return Database::queryFetchRow($getCountOfProductsQuery)['totalComments'];
     }
 
-    //todo #1 добавить потом вывод только промодерированных отзывов
-    public static function selectAllCommentsWithProductId($productId): array
-    {
-        $selectAllQuery = "select * from comments where product_id = $productId;";
-        $result = Database::queryFetchAll($selectAllQuery);
-
-        return EntityUtil::resultToListOfEntities("CommentEntity", $result);
-    }
-
+    //TODO #1 добавить вывод промодерированных отзывов
     public static function selectAllCommentsWithProductIdLimitOffset($productId, $limit, $offset): array
     {
         $selectAllQuery = "select * from comments where product_id = $productId limit $limit offset $offset;";
@@ -59,7 +51,6 @@ class Comments
 
     public static function insertNewComment(CommentEntity $comment): void
     {
-        //TODO №5 не понятно где именно нужно будет делать валидацию
         //TODO #6 так же тут где то нужно будет делать проверку на то что такой продукт существует
         $insertQuery = "insert into comments (email, comment, product_id) values (?, ?, ?)";
         Database::prepareAndExecute(
