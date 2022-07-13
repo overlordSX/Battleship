34ff5e46364d58d7d8e38fbba4ceb01e5a2e9ed3
andrey.solovev <stajer@sibirix.ru>
2022-07-13 17:15:35 +0700

    * сделал метод подключающий вью статическим

--- view/View.php
+++ view/View.php
@@ -2,7 +2,7 @@
 
 class View
 {
-    public function generateView(string $filepath, array $data = []): void
+    public static function generateView(string $filepath, array $data = []): void
     {
         extract($data);
         include $filepath;
