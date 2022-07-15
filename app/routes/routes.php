17f58422f93602e8f097f19b3b77d57f3c9cb88d
andrey.solovev <stajer@sibirix.ru>
2022-07-15 18:55:43 +0700

    * исправил reg_exp у роутов

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -1,48 +1,48 @@
 <?php
-Router::route(['/catalog/product/(\d+)', '/catalog/product/(\d+)/comments(.+)'], function (int $productId) {
+Router::route(['^/catalog/product/(\d+)$', '^/catalog/product/(\d+)/comments(.+)$'], function (int $productId) {
     ProductController::showProduct($productId);
 });
 
-Router::route(['/catalog/product/(\d+)/comment/new'], function (int $productId) {
+Router::route(['^/catalog/product/(\d+)/comment/new$'], function (int $productId) {
     CommentController::postNewComment($productId);
 });
 
-Router::route(['/catalog/product/new'], function () {
+Router::route(['^/catalog/product/new$'], function () {
     ProductController::newProduct();
 });
 
-Router::route(['/', '/catalog', '/catalog?(.+)'], function () {
+Router::route(['^/$', '^/catalog$', '^/catalog\?.+$'], function () {
     CatalogController::renderPage();
 });
 
-Router::route(['/error-404'], function () {
+Router::route(['^/error-404$'], function () {
     View::generateView('view/errors/404.php');
 });
 
-Router::route(['/error-500'], function () {
+Router::route(['^/error-500$'], function () {
     View::generateView("view/errors/500.php");
 });
 
-Router::route(['admin/catalog/drop'], function () {
+Router::route(['^/admin/catalog/drop$'], function () {
     CatalogController::dropTable();
 });
 
-Router::route(['admin/catalog/create'], function () {
+Router::route(['^/admin/catalog/create$'], function () {
     CatalogController::createTable();
 });
 
-Router::route(['admin/messages/drop'], function () {
+Router::route(['^/admin/messages/drop$'], function () {
     CommentController::dropTable();
 });
 
-Router::route(['admin/messages/create'], function () {
+Router::route(['^/admin/messages/create$'], function () {
     CommentController::createTable();
 });
 
-Router::route(['/admin', '/admin?(.+)'], function () {
+Router::route(['^/admin$', '^/admin\?.+$'], function () {
     AdminController::adminPanel();
 });
 
-Router::route(['/catalog/generate/(\d+)'], function ($quantity) {
+Router::route(['^/catalog/generate/(\d+)$'], function ($quantity) {
     CatalogController::generateProducts($quantity);
 });
