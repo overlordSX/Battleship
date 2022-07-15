d42cb04bf62e951b9e7f71c995f965e690ab16ee
andrey.solovev <stajer@sibirix.ru>
2022-07-15 12:27:26 +0700

    + добавил возможность отображать ошибки валидации

--- view/comments/comments.php
+++ view/comments/comments.php
@@ -4,7 +4,6 @@
  * @var int $productId
  * @var int $commentPage
  * @var int $totalPages
- * @var array $query
  */
 ?>
 <hr>
@@ -12,25 +11,51 @@
     <div class="row">
         <h4>Блок комментариев</h4>
     </div>
-
-
     <div class="card">
         <div class="card-body">
             <form action="/catalog/product/<?= $productId ?>/comment/new" method="post">
                 <div class="row row-cols-auto">
-                    <div class="col"><h5 class="card-title">Оставьте свой отзыв</h5></div>
                     <div class="col">
-                        <button type="submit" class="btn btn-primary">Отправить</button>
+                        <h5 class="card-title">Оставьте свой отзыв</h5>
                     </div>
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary" tabindex="3">Отправить</button>
+                    </div>
+                </div>
 
+                <div class="row">
+                    <?
+                    $fieldNames =
+                        [
+                            'email' => 'Ваша эл. почта',
+                            'comment' => 'Напишите свой отзыв'
+                        ];
+
+                    if (isset($fieldsErrors)) { ?>
+                        <div class="row justify-content-start">
+                            <div class="col-6">
+                                <? foreach ($fieldsErrors as $field => $message) { ?>
+                                    <div class="badge bg-danger text-wrap mt-2">
+                                        <p class="fs-6 m-0"><?= $fieldNames[$field] . ': ' . $message ?></p>
+                                    </div>
+                                    <?
+                                } ?>
+                            </div>
+                        </div>
+                        <?
+                    }
+                    ?>
                 </div>
                 <div class="mb-1">
-                    <label for="email" class="form-label">Ваша эл. почта</label>
-                    <input type="email" class="form-control" name="email" placeholder="name@example.com">
+                    <label for="email" id="labelEmail" class="form-label"><?= $fieldNames['email'] ?></label>
+                    <input type="email" class="form-control" id="labelEmail" name="email" tabindex="1"
+                           placeholder="name@example.com"
+                           value="<?= $trimPost['email'] ?? '' ?>">
                 </div>
                 <div class="mb-1">
-                    <label for="comment" class="form-label">Напишите свой отзыв</label>
-                    <textarea class="form-control" name="comment" rows="3"></textarea>
+                    <label for="comment" id="labelEmail" class="form-label"><?= $fieldNames['comment'] ?></label>
+                    <textarea id="labelComment" class="form-control" name="comment" tabindex="2"
+                              rows="3"><?= $trimPost['comment'] ?? '' ?></textarea>
                 </div>
             </form>
         </div>
@@ -65,10 +90,9 @@
                     'countOfPages' => $totalPages,
                     'currentPageNumber' => $commentPage,
                     'currentUrl' => 'catalog/product/' . $productId . "/comments",
-                    'query' => $query
+                    'getQuery' => $_GET
                 ]
             );
-
             ?>
         </div>
     </div>
--- view/product/new.php
+++ view/product/new.php
@@ -5,26 +5,70 @@ View::generateView(
 );
 ?>
 
-    <div class="container">
-        <div class="row">
-            <form action="" method="post">
-                <div class="row-3">
-                    <input type=" text" name="name" placeholder="Название товара" maxlength="200">
-                </div>
-                <div class="row-3">
-                    <input type="text" name="price" placeholder="Цена товара">
-                </div>
-                <div class="row-3">
-                    <textarea rows="10" cols="45" name="description" maxlength="500"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Создать</button>
-                <a href="/"><button type="button" class="btn btn-outline-secondary">Главная</button></a>
+    <div class="container pt-4">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div class="row pb-1">
+                    <?
+                    $fieldNames =
+                        [
+                            'name' => 'Название товара',
+                            'price' => 'Цена товара',
+                            'description' => 'Описание товара'
+                        ];
 
+                    if (isset($fieldsErrors)) { ?>
+                        <div class="row justify-content-start">
+                            <div class="col-8">
+                                <? foreach ($fieldsErrors as $field => $message) { ?>
+                                    <div class="badge bg-danger text-wrap mt-2">
+                                        <p class="fs-6 m-0"><?= $fieldNames[$field] . ': ' . $message ?></p>
+                                    </div>
+                                    <?
+                                } ?>
+                            </div>
+                        </div>
+                        <?
+                    }
+                    ?>
+                </div>
+                <form action="" method="post">
+                    <div class="row pb-1">
+                        <label id="labelName"><?= $fieldNames['name'] ?></label>
+                        <input class="form-control" id="labelName" type="text" name="name" minlength="3" maxlength="200"
+                               required
+                               placeholder="<?= $fieldNames['name'] ?>"
+                               value="<?= $trimPost['name'] ?? '' ?>">
+                    </div>
+                    <div class="row pb-1">
+                        <label id="labelPrice"><?= $fieldNames['price'] ?></label>
+                        <input class="form-control" name="price" id="labelPrice" type="number" min="1"
+                               max="999999999999" required
+                               placeholder="<?= $fieldNames['price'] ?>"
+                               value="<?= $trimPost['price'] ?? '' ?>">
+                    </div>
+                    <div class="row pb-3">
+                        <label id="labelDescription"><?= $fieldNames['description'] ?></label>
+                        <textarea class="form-control" rows="10" name="description" id="labelDescription" minlength="2"
+                                  maxlength="500" required
+                                  placeholder="<?= $fieldNames['description'] ?>"
+                        ><?= $trimPost['description'] ?? '' ?></textarea>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-3">
+                            <div class="row pb-3">
+                                <button type="submit" class="btn btn-primary">Добавить</button>
+                            </div>
+                            <div class="row">
+                                <a href="/" role="button" class="btn btn-outline-secondary">Главная</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
-            </form>
         </div>
     </div>
-
 
 <?
 View::generateView(
