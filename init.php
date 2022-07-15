ebda8cd38571f0f40ae0dbb72fbb8a6cf7a71e4c
andrey.solovev <stajer@sibirix.ru>
2022-07-15 12:29:07 +0700

    * изменил пути подключаемых файлов + добавил новые подключаемые файлы

--- init.php
+++ init.php
@@ -1,22 +1,32 @@
 <?php
 
-require_once "routes/Router.php";
-require_once "routes/routes.php";
+require_once "app/routes/Router.php";
+require_once "app/routes/routes.php";
 
-require_once "database/entity/AbstractEntity.php";
-require_once "database/entity/ProductEntity.php";
-require_once "database/entity/CommentEntity.php";
+require_once "app/database/entity/AbstractEntity.php";
+require_once "app/database/entity/ProductEntity.php";
+require_once "app/database/entity/CommentEntity.php";
 
-require_once "database/util/EntityUtil.php";
+require_once "app/database/util/EntityUtil.php";
 
-require_once "database/Products.php";
-require_once "database/Comments.php";
+require_once "app/database/Products.php";
+require_once "app/database/Comments.php";
 
-require_once "database/config/config.php";
-require_once "database/Database.php";
+require_once "app/database/config/config.php";
+require_once "app/database/Database.php";
 
 require_once "view/View.php";
 
-require_once "controllers/CatalogController.php";
-require_once "controllers/ProductController.php";
-require_once "controllers/CommentController.php";
+require_once "app/controllers/CatalogController.php";
+require_once "app/controllers/ProductController.php";
+require_once "app/controllers/CommentController.php";
+
+require_once "app/validator/Validator.php";
+require_once "app/validator/RuleInterface.php";
+require_once "app/validator/IsLenAllowed.php";
+require_once "app/validator/IsRequired.php";
+require_once "app/validator/IsString.php";
+require_once "app/validator/IsPosDigit.php";
+require_once "app/validator/IsEmail.php";
+
+
