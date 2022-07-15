7d32dcdade1c882cca0a22a8bfed3e9b57eb963e
andrey.solovev <stajer@sibirix.ru>
2022-07-15 17:38:43 +0700

    * исправил роуты

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -1,17 +1,4 @@
 <?php
-Router::route(['/'], function () {
-    echo "<br><a href='/catalog'><button>Каталог</button></a>";
-    echo "<br><a href='/catalog/create'><button>Создать таблицу Товары</button></a>";
-    echo "<br><a href='/catalog/drop'><button>Удалить таблицу Товары</button></a>";
-    echo "<br><a href='/catalog/generate/5'><button>Генерация 5 товаров</button></a>";
-    echo "<br><a href='/catalog/product/new'><button>Добавить Товар</button></a>";
-
-    echo "<br>";
-    echo "<br><a href='/messages/create'><button>Создать таблицу Отзывы</button></a>";
-    echo "<br><a href='/messages/drop'><button>Удалить таблицу Отзывы</button></a>";
-});
-
-
 Router::route(['/catalog/product/(\d+)', '/catalog/product/(\d+)/comments(.+)'], function (int $productId) {
     ProductController::showProduct($productId);
 });
@@ -24,16 +11,7 @@ Router::route(['/catalog/product/new'], function () {
     ProductController::newProduct();
 });
 
-// todo #1 это переделать в экшн кнопки
-Router::route(['/catalog/drop'], function () {
-    CatalogController::dropTable();
-});
-
-Router::route(['/catalog/create'], function () {
-    CatalogController::createTable();
-});
-
-Router::route(['/catalog', '/catalog?(.+)'], function () {
+Router::route(['/', '/catalog', '/catalog?(.+)'], function () {
     CatalogController::renderPage();
 });
 
@@ -45,16 +23,26 @@ Router::route(['/error-500'], function () {
     View::generateView("view/errors/500.php");
 });
 
+Router::route(['admin/catalog/drop'], function () {
+    CatalogController::dropTable();
+});
 
-Router::route(['/messages/drop'], function () {
+Router::route(['admin/catalog/create'], function () {
+    CatalogController::createTable();
+});
+
+Router::route(['admin/messages/drop'], function () {
     CommentController::dropTable();
 });
 
-Router::route(['/messages/create'], function () {
+Router::route(['admin/messages/create'], function () {
     CommentController::createTable();
 });
 
-// todo #2 переделать в мини формочку
+Router::route(['/admin', '/admin?(.+)'], function () {
+    AdminController::adminPanel();
+});
+
 Router::route(['/catalog/generate/(\d+)'], function ($quantity) {
     CatalogController::generateProducts($quantity);
 });
