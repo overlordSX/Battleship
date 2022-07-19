40eb2405728adad9365a0645122c68e53d1c94f2
andrey.solovev <stajer@sibirix.ru>
2022-07-19 15:22:21 +0700

    * пример использования QueryBuilder

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -10,17 +10,36 @@ class AdminController implements ControllerInterface
 
             $currentAdminPage = $_GET['page'] ?? 1;
             $offset = ($currentAdminPage - 1) * self::ADMIN_PAGE_SIZE;
-            $totalNotActiveComments = Comments::getCountOfAllNotActiveComments();
+            $qb = new QueryBuilder();
+            $totalNotActiveComments = $qb->
+            from('comments')->
+            where('activity_status', '=', 'false')->
+            selectRow('select count(*) as total_comments')->
+            fetchRow()['total_comments'];
 
-            /**
-             * @var ProductEntity $product
-             */
-            $nonModerateComments = Comments::getAllCommentsWithLimitOffsetNonModerate(self::ADMIN_PAGE_SIZE, $offset);
+
+            $nonModerateComments = [];
+            $qb->clear();
+
+            $nonModerateComments =
+                $qb->
+                from('comments')->
+                where('activity_status', '=', '0')->
+                select('*')->
+                limitOffset(':limit', ':offset')->
+                fetchAll(
+                    [
+                        'limit' => self::ADMIN_PAGE_SIZE,
+                        'offset' => $offset
+                    ]
+                );
+
+            $nonModerateComments = EntityUtil::resultToListOfEntities("CommentEntity", $nonModerateComments);
 
             View::generateView(
                 'view/admin/admin.php',
                 [
-                    'nonModerateComments' => $nonModerateComments,
+                    'nonModerateComments' => empty($nonModerateComments) ? [] : $nonModerateComments,
                     'currentAdminPage' => $currentAdminPage,
                     'totalPages' => ceil($totalNotActiveComments / self::ADMIN_PAGE_SIZE),
                 ]
