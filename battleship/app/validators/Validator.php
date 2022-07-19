fe386b062f76776b5a44e1591f22ee1da110f77e
andrey.solovev <stajer@sibirix.ru>
2022-07-19 15:45:38 +0700

    + валидатор

---
+++ battleship/app/validators/Validator.php
@@ -0,0 +1,38 @@
+<?php
+
+class Validator
+{
+    protected array $fieldsWithErrors = [];
+
+    /**
+     * @param array $fieldsData
+     * @param array $fieldsRules
+     * @return void
+     */
+    public function make(array $fieldsData, array $fieldsRules): void
+    {
+        /**
+         * @param RuleInterface $rule
+         */
+        foreach ($fieldsRules as $field => $rules) {
+            foreach ($rules as $rule) {
+                if (!$rule->pass($fieldsData[$field] ?? null)) {
+
+                    $this->fieldsWithErrors[$field] = $rule->message();
+                    break;
+                }
+
+            }
+        }
+    }
+
+    public function isValid(): bool
+    {
+        return empty($this->fieldsWithErrors);
+    }
+
+    public function getErrors(): array
+    {
+        return $this->fieldsWithErrors;
+    }
+}
