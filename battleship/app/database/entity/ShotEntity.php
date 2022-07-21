812334439ce72ecb83996248da8053e8655d72d8
andrey.solovev <stajer@sibirix.ru>
2022-07-21 18:37:53 +0700

    + сущность описывающая размещение выстрелов

---
+++ battleship/app/database/entity/ShotEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+class ShotEntity extends AbstractEntity
+{
+    protected ?int $id;
+    protected int $coordinate_x;
+    protected int $coordinate_y;
+    protected int $gameFieldId;
+
+
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCoordinateX(): int
+    {
+        return $this->coordinate_x;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCoordinateY(): int
+    {
+        return $this->coordinate_y;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGameFieldId(): int
+    {
+        return $this->gameFieldId;
+    }
+}
