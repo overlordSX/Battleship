98d5ebea1b6515eb011d9cd954d34fa61823717c
andrey.solovev <stajer@sibirix.ru>
2022-07-21 18:34:39 +0700

    + сущность описывающая сообщение

---
+++ battleship/app/database/entity/MessageEntity.php
@@ -0,0 +1,50 @@
+<?php
+
+class MessageEntity extends AbstractEntity
+{
+    protected ?int $id;
+    protected int $createdAt;
+    protected string $content;
+    protected int $gameId;
+    protected int $playerId;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCreatedAt(): int
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGameId(): int
+    {
+        return $this->gameId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPlayerId(): int
+    {
+        return $this->playerId;
+    }
+}
