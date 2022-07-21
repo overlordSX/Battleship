14537c6e07be720153874f61c31f07fc4f468679
andrey.solovev <stajer@sibirix.ru>
2022-07-21 18:33:23 +0700

    + сущность игрового статуса

---
+++ battleship/app/database/entity/GameStatusEntity.php
@@ -0,0 +1,32 @@
+<?php
+
+class GameStatusEntity extends AbstractEntity
+{
+    protected ?int $id;
+    protected int $status;
+    protected string $description;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}
