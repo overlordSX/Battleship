55efd5c44401b0a7923cb4b520945f7960dac86c
andrey.solovev <stajer@sibirix.ru>
2022-07-21 18:39:53 +0700

    + параметр глубина

--- battleship/app/controllers/util/JsonUtil.php
+++ battleship/app/controllers/util/JsonUtil.php
@@ -2,10 +2,10 @@
 
 class JsonUtil
 {
-    public static function makeAnswer(array $data): void
+    public static function makeAnswer(array $data, int $depth = 512): void
     {
         header('Content-Type: application/json');
-        echo json_encode($data);
+        echo json_encode($data, $depth);
         die();
     }
 
