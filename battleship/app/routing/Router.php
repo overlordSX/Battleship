b294797d3edc19e89bc5d185290a888b6025edbe
andrey.solovev <stajer@sibirix.ru>
2022-07-22 18:41:26 +0700

    * добавил в роутер параметр отвечающий за тип запроса

--- battleship/app/routing/Router.php
+++ battleship/app/routing/Router.php
@@ -24,44 +24,40 @@ class Router
      * @param array $classNameAndMethod
      * @throws Exception
      */
-    public static function route(array $patternArray, array $classNameAndMethod): void
+    public static function route(string $requestedMethod, array $patternArray, array $classNameAndMethod): void
     {
-        $className = $classNameAndMethod[0];
+        $class = new $classNameAndMethod[0];
         $method = $classNameAndMethod[1];
-        //var_dump($patternArray, $className, $method);
-        //var_dump(is_subclass_of($className, ControllerInterface::class));
-        if (!is_subclass_of($className, ControllerInterface::class)) {
+        if (!is_subclass_of($class, ControllerInterface::class)) {
             throw new Exception('Такого контроллера нет');
         }
 
-        $class = new $className;
-
         foreach ($patternArray as $pattern) {
             $pattern = '/^' . str_replace('/', '\/', $pattern) . '$/';
-            //self::$routes[$pattern] = $class->$method;
-            self::$routes[$pattern] = [$class, $method];
+
+            self::$routes[$pattern] = ['callback' => [$class, $method], 'requestedMethod' => $requestedMethod];
         }
     }
 
 
     /**
-     * @var string $url заправшиваемый url
-     * @var callable $callback
      * @return mixed|void
+     * @var callable $callback
+     * @var string $url заправшиваемый url
      */
     public static function execute(string $url)
     {
         //echo $url . '<br>';
 
         $found = false;
-        foreach (self::$routes as $pattern => $callback) {
-            if (preg_match($pattern, $url, $params)) {
+        foreach (self::$routes as $pattern => $routeParams) {
+            if (preg_match($pattern, $url, $params) and $routeParams['requestedMethod'] === $_SERVER['REQUEST_METHOD']) {
                 array_shift($params);
-                //echo $callback;
+                //echo $routeParams;
 
                 //var_dump( $params);
                 $found = true;
-                return call_user_func_array($callback, array_values($params));
+                return call_user_func_array($routeParams['callback'], array_values($params));
             }
         }
         /*if (!$found) {
