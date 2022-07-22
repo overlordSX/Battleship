f3475c2eb179d25d6a7abf193670f431ae7c4d83
andrey.solovev <stajer@sibirix.ru>
2022-07-22 17:42:24 +0700

    * подправил данные для создания таблиц, game_status & ships

--- battleship/app/controllers/DatabaseController.php
+++ battleship/app/controllers/DatabaseController.php
@@ -2,13 +2,11 @@
 
 class DatabaseController implements ControllerInterface
 {
-public function createTables(): void
-{
-    // TODO переделать game, на хранение двух id
-
-$queryBuilder = new QueryBuilder();
-var_dump($queryBuilder->
-selectRow('
+    public function createTables(): void
+    {
+        $queryBuilder = new QueryBuilder();
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists game_status
         (
             id int not null auto_increment,
@@ -17,9 +15,9 @@ selectRow('
             primary key (id)
         ) engine = InnoDB;
 ')->
-prepareAndExecute());
-var_dump($queryBuilder->
-selectRow('
+        prepareAndExecute());
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists player
 (
 	id int not null auto_increment,
@@ -27,26 +25,28 @@ selectRow('
 	primary key (id)
 ) engine = InnoDB;
 ')->
-prepareAndExecute());
+        prepareAndExecute());
 //TODO тут нужно сделать invite_code уникальным, да и id игрока тоже
-var_dump($queryBuilder->
-selectRow('
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists game
 (
 	id int not null auto_increment,
-	invite_code varchar(32) not null,
 	turn bool not null,
 	game_status_id int not null,
-	player_id int not null,
+	first_player_id int not null,
+	second_player_id int not null,
 
 	primary key (id),
 	foreign key (game_status_id) references game_status(id),
-	foreign key (player_id) references player(id)
+	foreign key (first_player_id) references player(id),
+	foreign key (second_player_id) references player(id)
 ) engine = InnoDB;
+
 ')->
-prepareAndExecute());
-var_dump($queryBuilder->
-selectRow('
+        prepareAndExecute());
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists message
 (
 	id int not null auto_increment,
@@ -60,21 +60,21 @@ selectRow('
 	foreign key (player_id) references player(id)
 ) engine = InnoDB;
 ')->
-prepareAndExecute());
-var_dump($queryBuilder->
-selectRow('
+        prepareAndExecute());
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists ship
 (
 	id int not null auto_increment,
+	name varchar(10) not null,
 	size int not null,
-	quantity int not null,
 
 	primary key (id)
 ) engine = InnoDB;
 ')->
-prepareAndExecute());
-var_dump($queryBuilder->
-selectRow('
+        prepareAndExecute());
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists game_field
 (
 	id int not null auto_increment,
@@ -87,9 +87,9 @@ selectRow('
 	foreign key (player_id) references player(id)
 ) engine = InnoDB;
 ')->
-prepareAndExecute());
-var_dump($queryBuilder->
-selectRow('
+        prepareAndExecute());
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists ship_placement
 (
 	id int not null auto_increment,
@@ -104,9 +104,9 @@ selectRow('
 	foreign key (game_field_id) references game_field(id)
 ) engine = InnoDB;
 ')->
-prepareAndExecute());
-var_dump($queryBuilder->
-selectRow('
+        prepareAndExecute());
+        var_dump($queryBuilder->
+        selectRow('
         create table if not exists shot
 (
 	id int not null auto_increment,
@@ -118,110 +118,108 @@ selectRow('
 	foreign key (game_field_id) references game_field(id)
 ) engine = InnoDB;
 ')->
-prepareAndExecute());
+        prepareAndExecute());
 
 
-/*echo '<br>';
+        /*echo '<br>';
 
-$table = [
-    'game_status',
-    'player',
-    'game',
-    'message',
-    'ship',
-    'game_field',
-    'ship_placement',
-    'shot'
-];
+        $table = [
+            'game_status',
+            'player',
+            'game',
+            'message',
+            'ship',
+            'game_field',
+            'ship_placement',
+            'shot'
+        ];
 
-foreach ($table as $name) {
-    echo $name . "<br>";
-    $columns = $queryBuilder
-        ->selectRow('SHOW columns from ' . $name )->fetchAll();
-    //print_r($columns);
-    $requiredFields = [];
-    foreach ($columns as $row) {
-        //var_dump($row['Extra'] !== 'auto_increment');
-        if (empty($row['Extra'])) {
-            $requiredFields[] = $row['Field'];
+        foreach ($table as $name) {
+            echo $name . "<br>";
+            $columns = $queryBuilder
+                ->selectRow('SHOW columns from ' . $name )->fetchAll();
+            //print_r($columns);
+            $requiredFields = [];
+            foreach ($columns as $row) {
+                //var_dump($row['Extra'] !== 'auto_increment');
+                if (empty($row['Extra'])) {
+                    $requiredFields[] = $row['Field'];
+                }
+            }
+
+            print_r($requiredFields);
+            echo '<br>';
+        }*/
+
+        $shipModel = new ShipModel();
+
+        $shipName =
+            [
+                '1-1',
+                '1-2',
+                '1-3',
+                '1-4',
+                '2-1',
+                '2-2',
+                '2-3',
+                '3-1',
+                '3-2',
+                '4-1'
+            ];
+
+        /*foreach ($shipName as $name) {
+            $shipModel
+                ->clear()
+                ->insert(['name' => $name, 'size' => substr($name, 0, 1)]);
+        }*/
+
+
+        header('Content-Type: application/json');
+
+        var_dump($shipModel->clear()->query()->select('*')->fetchAll());
+
+        $gameStatusModel = new GameStatusModel();
+
+        $gameStatuses =
+            [
+                '1' => 'Расстановка кораблей. Начало игры.',
+                '2' => 'Идет игра.',
+                '3' => 'Игра закончена.'
+            ];
+
+        foreach ($gameStatuses as $status => $description) {
+            $gameStatusModel
+                ->clear()
+                ->insert(['status' => $status, 'description' => $description]);
         }
+
+        var_dump($gameStatusModel->clear()->query()->select('*')->fetchAll());
+
+
+        //TODO DELETE
+        //var_dump($queryBuilder->clear()->selectRow('drop table if exists shot, ship_placement, game_field, ship, message, game, player, game_status')->prepareAndExecute());
+
+        //QueryBuilderUtil::getTableColumns('player')
+
+
+        /*$tableNames =
+            [
+                'player',
+                'game_status',
+                'game',
+                'message',
+                'ship',
+                'ship_placement',
+                'game_field',
+                'shot'
+            ];
+        $getQuery = [];
+        foreach (['create', 'drop'] as $type) {
+            $getQuery['action'] = $type;
+            foreach ($tableNames as $tableName) {
+                $getQuery['table'] = $tableName;
+        */
+
     }
-
-    print_r($requiredFields);
-    echo '<br>';
-}*/
-
-$shipModel = new ShipModel();
-/*$shipModel
-    ->insert(['size' => 1, 'quantity' => 4]);
-$shipModel
-    ->clear()
-    ->insert(['size' => 2, 'quantity' => 3]);
-$shipModel
-    ->clear()
-    ->insert(['size' => 3, 'quantity' => 2]);
-$shipModel
-    ->clear()
-    ->insert(['size' => 4, 'quantity' => 1]);*/
-
-print_r($shipModel->clear()->query()->select('*')->fetchAll());
-
-$gameStatusModel = new GameStatusModel();
-
-//$gameStatusModel->insert([[1, 'Расстановка кораблей. Начало игры.'], [2, 'Идет игра.'], [3, 'Игра закончена']]);
-/*$gameStatusModel
-    ->insert(['status' => 1, 'description' => 'Расстановка кораблей. Начало игры.']);
-$gameStatusModel
-    ->clear()
-    ->insert(['status' => 1, 'description' => 'Расстановка кораблей. Начало игры.']);
-$gameStatusModel
-    ->clear()
-    ->insert(['status' => 1, 'description' => 'Расстановка кораблей. Начало игры.']);*/
-
-print_r($gameStatusModel->clear()->query()->select('*')->fetchAll());
-
-//var_dump(!is_subclass_of(PlayerEntity::class, AbstractEntity::class));
-
-//TODO DELETE
-//var_dump($queryBuilder->clear()->selectRow('drop table if exists shot, ship_placement, game_field, ship, message, game, player, game_status')->prepareAndExecute());
-
-//QueryBuilderUtil::getTableColumns('player')
-
-
-/*$tableNames =
-    [
-        'player',
-        'game_status',
-        'game',
-        'message',
-        'ship',
-        'ship_placement',
-        'game_field',
-        'shot'
-    ];
-$getQuery = [];
-foreach (['create', 'drop'] as $type) {
-    $getQuery['action'] = $type;
-    foreach ($tableNames as $tableName) {
-        $getQuery['table'] = $tableName;
-
-        */ ?><!--
-                <div class="col-3 pt-1">
-                    <a role="button" class="btn btn-warning"
-                       href="<?/*= '/db?' . http_build_query($getQuery) */ ?>">
-                        <button><?/*= $type . ' : ' . $tableName */ ?></button>
-                    </a>
-                </div>
-            <?/* }
-        } */ ?>
-
-        <div class="col-3 pt-1">
-            <a role="button" class="btn btn-warning"
-               href="<?/*= '/db' */ ?>">
-                <button>Сброс параметроВ</button>
-            </a>
-        </div>
-        --><?
-}
 
 }
