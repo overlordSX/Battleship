727d353404cb920a0b88383cca24b67a249395a1
andrey.solovev <stajer@sibirix.ru>
2022-07-22 18:41:51 +0700

    * добавил в роуты параметр отвечающий за тип запроса

--- battleship/app/routing/routes.php
+++ battleship/app/routing/routes.php
@@ -1,22 +1,20 @@
 <?
-//TODO в метод роут можно добавить параметр, который будет отвечать за тип POST|GET
+Router::route('GET', ['/db?(.+)'], [DatabaseController::class, 'createTables']);
 
-Router::route(['/db?(.+)'], [DatabaseController::class, 'createTables']);
-
-Router::route(['/api/start/'], [GameController::class, 'startNewGame']);
+Router::route('POST', ['/api/start/'], [GameController::class, 'startNewGame']);
 
 //TODO из фронта просит запрос без /, а в ТЗ со /
-Router::route(['/api/status/(\d+)/(.+)'], [GameController::class, 'getStatus']);
+Router::route('POST', ['/api/status/(\d+)/(.+)'], [GameController::class, 'getStatus']);
 
-Router::route(['/api/place-ship/(\d+)/(.+)'], [PlacementController::class, 'placeShip']);
+Router::route('POST', ['/api/place-ship/(\d+)/(.+)'], [PlacementController::class, 'placeShip']);
 
-Router::route(['/api/clear-field/(\d+)/(.+)/'], [PlacementController::class, 'clearField']);
+Router::route('POST', ['/api/clear-field/(\d+)/(.+)/'], [PlacementController::class, 'clearField']);
 
-Router::route(['/api/ready/(\d+)/(.+)/'], [GameController::class, 'setStatus']);
+Router::route('POST', ['/api/ready/(\d+)/(.+)/'], [GameController::class, 'setStatus']);
 
-Router::route(['/api/shot/(\d+)/(.+)/'], [ShotController::class, 'makeShot']);
+Router::route('POST', ['/api/shot/(\d+)/(.+)/'], [ShotController::class, 'makeShot']);
 
-Router::route(['/api/chat-load/(\d+)/(.+)/'], [ChatController::class, 'loadChat']);
+Router::route('GET', ['/api/chat-load/(\d+)/(.+)/'], [ChatController::class, 'loadChat']);
 
-Router::route(['/api/chat-send/(\d+)/(.+)/'], [ChatController::class, 'sendMessage']);
+Router::route('POST', ['/api/chat-send/(\d+)/(.+)/'], [ChatController::class, 'sendMessage']);
 
