27c7434cdd5ab6c98b45a5984a03bf6dea75cde5
andrey.solovev <stajer@sibirix.ru>
2022-07-25 18:00:51 +0700

    + функция возвращающая игрока по: id, code

--- Battleship/App/Database/Models/PlayerModel.php
+++ Battleship/App/Database/Models/PlayerModel.php
@@ -2,6 +2,7 @@
 
 namespace Battleship\App\Database\Model;
 
+use Battleship\App\Database\Entity\AbstractEntity;
 use Battleship\App\Database\Entity\PlayerEntity;
 
 /**
@@ -12,4 +13,36 @@ class PlayerModel extends AbstractModel
 {
     protected string $tableName = 'player';
     protected string $entityClassName = PlayerEntity::class;
+
+
+    /**
+     * @param $playerCode
+     * @return PlayerEntity
+     * @throws \Exception
+     */
+    public function getPlayerByCode($playerCode): AbstractEntity
+    {
+
+        return $this
+            ->query()
+            ->where('code', '=', $playerCode)
+            ->select()
+            ->fetch();
+    }
+
+    /**
+     * @param $playerId
+     * @return PlayerEntity
+     * @throws \Exception
+     */
+    public function getPlayerById($playerId): AbstractEntity
+    {
+
+        return $this
+            ->query()
+            ->where('id', '=', $playerId)
+            ->select()
+            ->fetch();
+    }
+
 }
