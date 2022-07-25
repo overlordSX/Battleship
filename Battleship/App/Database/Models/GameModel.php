f7215ce0ebf45a705ef573f049d0d63095eb6eb1
andrey.solovev <stajer@sibirix.ru>
2022-07-25 18:00:20 +0700

    + функция возвращающая игру по: id, player_id + функция создающая новую игру

--- Battleship/App/Database/Models/GameModel.php
+++ Battleship/App/Database/Models/GameModel.php
@@ -2,7 +2,9 @@
 
 namespace Battleship\App\Database\Model;
 
+use Battleship\App\Database\Entity\AbstractEntity;
 use Battleship\App\Database\Entity\GameEntity;
+use Exception;
 
 /**
  * Аттрибуты:
@@ -15,4 +17,54 @@ class GameModel extends AbstractModel
 {
     protected string $tableName = 'game';
     protected string $entityClassName = GameEntity::class;
+
+    /**
+     * @param $firstPlayerId
+     * @param $secondPlayerId
+     * @return bool
+     * @throws Exception
+     */
+    public function createNewGame($firstPlayerId, $secondPlayerId): bool
+    {
+        return $this
+            ->insert([
+                'turn' => $this->getRandomTurn(),
+                'game_status_id' => 1,
+                'first_player_id' => $firstPlayerId,
+                'second_player_id' => $secondPlayerId
+            ]);
+    }
+
+    /**
+     * @param $playerId
+     * @return GameEntity
+     * @throws Exception
+     */
+    public function getGameByPlayerId($playerId): AbstractEntity
+    {
+        return $this
+            ->query()
+            ->select('id')
+            ->where('first_player_id', '=', $playerId)
+            ->fetch();
+    }
+
+    /**
+     * @param $gameId
+     * @return GameEntity
+     * @throws Exception
+     */
+    public function getGameById($gameId): AbstractEntity
+    {
+        return $this
+            ->query()
+            ->select()
+            ->where('id', '=', $gameId)
+            ->fetch();
+    }
+
+    protected function getRandomTurn(): bool
+    {
+        return rand(-100, 100) > 0;
+    }
 }
