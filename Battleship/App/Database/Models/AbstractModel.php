cca39852f6f2e3f3c5388a16225f5e3c3798d119
andrey.solovev <stajer@sibirix.ru>
2022-07-25 16:28:24 +0700

    * добавил параметров для where & update

--- Battleship/App/Database/Models/AbstractModel.php
+++ Battleship/App/Database/Models/AbstractModel.php
@@ -36,18 +36,18 @@ abstract class AbstractModel
 
     /**
      * @param string $attribute
+     * @param string $condition
      * @param array $oldValue
      * @param array $newValue
      * @return bool успешно или нет
-     * @throws Exception
      */
-    public function update(string $attribute, mixed $oldValue, mixed $newValue): bool
+    public function update(string $attribute, string $condition, mixed $oldValue, mixed $newValue): bool
     {
-        return $this->getQueryBuilder()->update($this->tableName, $attribute, $oldValue, $newValue);
+        return $this->getQueryBuilder()->update($this->tableName, $attribute, $condition, $oldValue, $newValue);
     }
 
-    public function delete(string $attribute, mixed $value): bool
+    public function delete(string $attribute, string $condition, mixed $value): bool
     {
-        return $this->getQueryBuilder()->delete($this->tableName, $attribute, $value);
+        return $this->getQueryBuilder()->delete($this->tableName, $attribute, $condition, $value);
     }
 }
