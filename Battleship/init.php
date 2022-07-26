67463e3aac0f0155aaaa5b2b4f4766eac8939840
andrey.solovev <stajer@sibirix.ru>
2022-07-26 18:28:10 +0700

    + подключил модель выстрелов

--- Battleship/init.php
+++ Battleship/init.php
@@ -27,6 +27,7 @@ Autoloader::register('Battleship/App/Database/Models/GameModel');
 Autoloader::register('Battleship/App/Database/Models/ShipModel');
 Autoloader::register('Battleship/App/Database/Models/GameFieldModel');
 Autoloader::register('Battleship/App/Database/Models/ShipPlacementModel');
+Autoloader::register('Battleship/App/Database/Models/ShotModel');
 
 
 Autoloader::register('Battleship/App/Controllers/Util/JsonUtil');
