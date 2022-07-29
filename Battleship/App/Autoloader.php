6760bcdd52060f38dbc85ed689edbf062c07f208
andrey.solovev <stajer@sibirix.ru>
2022-07-29 16:45:27 +0700

    * сжал форматирование * убрал throw

--- Battleship/App/Autoloader.php
+++ Battleship/App/Autoloader.php
@@ -8,29 +8,23 @@ class Autoloader
 {
     public static function register($path): void
     {
-        spl_autoload_register(
-        /**
-         * @throws Exception
-         */
-            function () use ($path) {
-
-                $filePath = $path . '.php';
-                try {
-                    require_once $filePath;
-                } catch (Exception $exception) {
-                    if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/logs/autoloader.log')) {
-                        mkdir($_SERVER['DOCUMENT_ROOT'] . '/logs');
-                        $mkFile = fopen($_SERVER['DOCUMENT_ROOT'] . '/logs/autoloader.log', "a+");
-                        fclose($mkFile);
-                    }
-                    error_log(
-                        $exception->getMessage() . PHP_EOL,
-                        3,
-                        $_SERVER['DOCUMENT_ROOT'] . '/logs/autoloader.log'
-                    );
-                    die();
+        spl_autoload_register(function () use ($path) {
+            $filePath = $path . '.php';
+            try {
+                require_once $filePath;
+            } catch (Exception $exception) {
+                if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/logs/autoloader.log')) {
+                    mkdir($_SERVER['DOCUMENT_ROOT'] . '/logs');
+                    $mkFile = fopen($_SERVER['DOCUMENT_ROOT'] . '/logs/autoloader.log', "a+");
+                    fclose($mkFile);
                 }
+                error_log(
+                    $exception->getMessage() . PHP_EOL,
+                    3,
+                    $_SERVER['DOCUMENT_ROOT'] . '/logs/autoloader.log'
+                );
+                die();
             }
-        );
+        });
     }
 }
