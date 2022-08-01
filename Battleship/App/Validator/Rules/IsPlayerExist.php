1ccc067acb23e5561efec199b27c765ea5f5ddd9
andrey.solovev <stajer@sibirix.ru>
2022-08-01 18:53:33 +0700

    + правило проверки того, что игрок существует

---
+++ Battleship/App/Validator/Rules/IsPlayerExist.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Battleship\App\Validator\Rule;
+
+use Battleship\App\Database\Model\PlayerModel;
+use Battleship\App\Validator\RuleInterface;
+
+class IsPlayerExist implements RuleInterface
+{
+
+    /** @throws \Exception */
+    public function pass($value): bool
+    {
+        $playerModel = new PlayerModel();
+        return $playerModel->query()
+            ->where('code', '=', $value)
+            ->fetchCount();
+    }
+
+    public function message(): string
+    {
+        return 'Игрока с таким кодом не существует';
+    }
+}
