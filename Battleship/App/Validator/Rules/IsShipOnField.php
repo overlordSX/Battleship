3541d8e112842fff258df64018cc182f3a296c31
andrey.solovev <stajer@sibirix.ru>
2022-08-03 10:10:24 +0700

    + правило проверки существования корабля на поле

---
+++ Battleship/App/Validator/Rules/IsShipOnField.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Battleship\App\Validator\Rule;
+
+use Battleship\App\Database\Model\GameFieldModel;
+use Battleship\App\Database\Model\GameModel;
+use Battleship\App\Database\Model\PlayerModel;
+use Battleship\App\Database\Model\ShipPlacementModel;
+use Battleship\App\Validator\RuleInterface;
+
+class IsShipOnField implements RuleInterface
+{
+
+    /**
+     * @throws \Exception
+     */
+    public function pass($value): bool
+    {
+        $gameId = $value['gameId'];
+        $playerCode = $value['playerCode'];
+        $shipName = $value['shipName'];
+
+        $game = (new GameModel())->getGameById($gameId);
+
+        $playerModel = new PlayerModel();
+        $player = $playerModel->getPlayerByCode($playerCode);
+
+        $gameField = (new GameFieldModel())->getByGameAndPlayer($game->getId(), $player->getId());
+
+        $shipPlacement = new ShipPlacementModel();
+        $placedShips = $shipPlacement->getPlacedShipsList($gameField->getId());
+
+        foreach ($placedShips as $ship) {
+            if ($ship->getCustom('name') === $shipName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function message(): string
+    {
+        return 'Данный корабль не находится на поле';
+    }
+}
