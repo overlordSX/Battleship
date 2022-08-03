9ecae183f9e354f884314bb2358abad25f1e3c6e
andrey.solovev <stajer@sibirix.ru>
2022-08-03 10:09:52 +0700

    + правило означающее обязательность параметра

---
+++ Battleship/App/Validator/Rules/IsRequired.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Battleship\App\Validator\Rule;
+
+use Battleship\App\Validator\RuleInterface;
+
+class IsRequired implements RuleInterface
+{
+    public function __construct(protected string $paramName)
+    {
+    }
+
+    public function pass($value): bool
+    {
+        return isset($value);
+    }
+
+    public function message(): string
+    {
+        return $this->paramName . ' является обязательным параметром';
+    }
+}
