96411a6f8a67710b728ef5fb45a658b4e3cf6094
andrey.solovev <stajer@sibirix.ru>
2022-08-03 09:49:42 +0700

    + валидация выстрела

--- Battleship/App/Controllers/ShotController.php
+++ Battleship/App/Controllers/ShotController.php
@@ -5,18 +5,27 @@ namespace Battleship\App\Controllers;
 
 use Battleship\App\Controllers\Util\JsonUtil;
 use Battleship\App\Database\Model\ShotModel;
+use Battleship\App\Validator\Request\MakeShotRequest;
 
 class ShotController implements ControllerInterface
 {
     /**
      * @throws \Exception
      */
-    public function makeShot($gameId, $playerCode)
+    public function makeShot(int $gameId, string $playerCode)
     {
+        $makeShotRequest = new MakeShotRequest();
+        $makeShotRequest->validate(['gameId' => $gameId, 'playerCode' => $playerCode]);
+        $requestAnswer = $makeShotRequest->answer();
+
+        if ($requestAnswer) {
+            JsonUtil::makeAnswer($requestAnswer);
+        }
+
         $shotModel = new ShotModel();
         $success = $shotModel->makeShot($gameId, $playerCode);
 
-         JsonUtil::makeAnswer($success);
+        JsonUtil::makeAnswer($success);
     }
 
 }
