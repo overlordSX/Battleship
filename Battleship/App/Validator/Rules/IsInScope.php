20d2367ce472fd2afb64a0c298bc185419007fbd
andrey.solovev <stajer@sibirix.ru>
2022-08-03 10:07:21 +0700

    + правило проверки значения что она находится в границах от min до max

---
+++ Battleship/App/Validator/Rules/IsInScope.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Battleship\App\Validator\Rule;
+
+use Battleship\App\Validator\RuleInterface;
+
+class IsInScope implements RuleInterface
+{
+    /**
+     * от min до max, включительно
+     * @param int $min
+     * @param int $max
+     */
+    public function __construct(protected int $min, protected int $max)
+    {
+    }
+
+    public function pass($value): bool
+    {
+        return $value >= $this->min && $value <= $this->max;
+    }
+
+    public function message(): string
+    {
+        return 'Координата должна быть от ' . $this->min . ', до ' . $this->max;
+    }
+}
