37fe72d6cba8dc13f0de1e99916a4b2dcab9fac7
andrey.solovev <stajer@sibirix.ru>
2022-08-03 10:04:52 +0700

    + правило проверки статуса игры

---
+++ Battleship/App/Validator/Rules/IsCorrectGameStatus.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Battleship\App\Validator\Rule;
+
+use Battleship\App\Database\Model\GameModel;
+use Battleship\App\Database\Model\GameStatusModel;
+use Battleship\App\Validator\RuleInterface;
+
+class IsCorrectGameStatus implements RuleInterface
+{
+    public function __construct(protected int $gameStatus)
+    {
+    }
+
+    public function pass($value): bool
+    {
+        $gameId = $value['gameId'];
+
+        $gameModel = new GameModel();
+        $game = $gameModel->getGameById($gameId);
+
+        if ($game->getGameStatusId() !== $this->gameStatus) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function message(): string
+    {
+        $gameStatusModel = new GameStatusModel();
+        $status = $gameStatusModel->getStatus($this->gameStatus);
+        return 'Этап: ' . $status->getDescription() . ': - уже прошел';
+    }
+}
