96f15d10f39f9ab30106f73395f65d33540b4c32
andrey.solovev <stajer@sibirix.ru>
2022-08-03 10:11:10 +0700

    + правило проверки очереди игрока

---
+++ Battleship/App/Validator/Rules/IsThisPlayerTurn.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Battleship\App\Validator\Rule;
+
+use Battleship\App\Database\Model\GameModel;
+use Battleship\App\Database\Model\PlayerModel;
+use Battleship\App\Validator\RuleInterface;
+
+class IsThisPlayerTurn implements RuleInterface
+{
+
+    public function pass($value): bool
+    {
+        $gameId = $value['gameId'];
+        $playerCode = $value['playerCode'];
+
+
+        $playerModel = new PlayerModel();
+        $game = (new GameModel())->getGameById($gameId);
+        $player = $playerModel->getPlayerByCode($playerCode);
+
+        if (!$playerModel->isMyTurn($game, $player)) {
+            return false;
+        }
+        return true;
+    }
+
+    public function message(): string
+    {
+        return 'Дождитесь своей очереди';
+    }
+}
