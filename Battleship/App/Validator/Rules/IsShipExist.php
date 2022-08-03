726dbb5f2f3e82bd8288baa5ab1ab46f6fd2ad2d
andrey.solovev <stajer@sibirix.ru>
2022-08-03 10:10:12 +0700

    + правило проверки существования корабля в БД

---
+++ Battleship/App/Validator/Rules/IsShipExist.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Battleship\App\Validator\Rule;
+
+use Battleship\App\Database\Model\ShipModel;
+use Battleship\App\Validator\RuleInterface;
+
+class IsShipExist implements RuleInterface
+{
+
+    /** @throws \Exception */
+    public function pass($value): bool
+    {
+        $shipName = $value;
+
+        return (new ShipModel())->isShipExist($shipName);
+    }
+
+    public function message(): string
+    {
+        return 'Такого корабля не существует';
+    }
+}
